3e10ba2528d1dc3a0ceff4929c0e726d52fda7cf
aviraj5421 <avirajschilgunde@gmail.com>
2026-09-23 17:46:56 +0530

    update profile api is completed

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/utils.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
 
@@ -98,7 +99,7 @@ export const login=async(req,res)=>{
         return res.status(400).json({
             message:"Email and password are required"
         })
-        
+
     }
 
     try {
@@ -151,5 +152,42 @@ export const logout=async(req,res)=>{
         message:"Logged out successfully"
     })
 
+
+}
+
+export const updateProfile=async(req,res)=>{
+
+    try {
+
+        const profilePic=req.body;
+
+        if(!profilePic){
+            return res.status(400).json({
+                message:"Profile picture is required"
+            })
+        }
+
+        const userId=req.user._id;
+
+        const uploadResponse=await cloudinary.uploader.upload(profilePic);
+
+        const updateUser=await User.findByIdAndUpdate(userId,{
+            profilePic:uploadResponse.secure_url
+        },{new:true});
+
+        
+        
+
+
+    }
+
+    catch (error) {
+    
+        console.error("Error in updateProfile:", error);
+        return res.status(500).json({
+            message: "Internal server error"
+        });
+
+    }
 
 }
